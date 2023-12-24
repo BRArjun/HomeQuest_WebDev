ca97a77794f5fc48fdc5b8bca30a2d75f1185a39
B R Arjun <brarjunkunjithaya@gmail.com>
2023-12-24 12:13:29 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+import cors from 'cors';
+
+mongoose
+  .connect(`${process.env.MONGO}`)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
+
+const app = express();
+app.use(cors({ origin: '*' }));
+
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client','index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
